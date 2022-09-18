c72c49209508950c984596bb9207595a59d7f4bd
Steven <stevenmontecinosce@gmail.com>
2022-09-17 23:11:21 -0400

    Basic sorting algorithms.

--- src/ArraySortingAlgorithms/QuickSort.java
+++ src/ArraySortingAlgorithms/QuickSort.java
@@ -5,7 +5,6 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-        //int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22, 2, 4 };
         quickSort(intArray);
 
